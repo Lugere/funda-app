bb26347122d6c7cd63a899c59fe43644ce00fe76
Sebastian Missler <Sebastian.Missler@itacsoftware.com>
2020-11-17 23:44:08 +0100

    implemented user-page

---
+++ src/views/Main/Children/Users/Users.ts
@@ -0,0 +1,121 @@
+import { Component, Vue, Watch } from "vue-property-decorator";
+import Vuex, { mapState } from "vuex";
+import store from "@/store";
+import moment from "moment";
+import getterMixin from "@/mixins/getterMixin";
+
+@Component({
+  computed: {
+    ...mapState(["users"]),
+  },
+  filters: {
+    trimLength(value: any): string {
+      if (!value) return "-";
+      if (value.length < 60) return value;
+      return `${value.slice(0, 60)}...`;
+    },
+    formatDate(value: any): string {
+      if (!value) return "-";
+      const DATE = new Date(value * 1000);
+      return moment(DATE).calendar();
+    },
+    formatCommentDate(value: any): string {
+      if (!value) return "-";
+      const DATE = new Date(value * 1000);
+      return moment(DATE).fromNow();
+    },
+  },
+})
+export default class Users extends getterMixin {
+  /* Store Bindings */
+  public users!: any;
+
+  /* Data */
+  // Subject Select
+  currentSubject = 0;
+
+  // Table Search
+  public search = "";
+  public searched: any = [];
+
+  // Table Item Select
+  public selected = [
+    {
+      userId: 0,
+    },
+  ];
+  public showNewUser = false;
+  public newUser = {
+    user_id: 0,
+    createdAt: moment().unix(),
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    role: 1,
+    username: "",
+  };
+  public resetNewUser: any = {};
+
+  // Table Empty State Messages
+  public noUsersTxt = `Es existieren noch keine Fragen im Katalog`;
+  public termNotFound = `Keine Fragen gefunden, die '${this.search}' enthalten.`;
+
+  /* Methods */
+  public searchByUser(items, term): any {
+    if (!term) return items;
+    const toLower = (text) => text.toString().toLowerCase();
+    return items.filter((item) =>
+      toLower(item.username).includes(toLower(term))
+    );
+  }
+
+  public searchOnTable(): void {
+    this.searched = this.searchByUser(this.users, this.search);
+  }
+
+  public getAlternateLabel(count): string {
+    return `${count == this.users.length ? "Alle" : count} Benuter
+        } ausgwählt`;
+  }
+
+  public deleteUsers(): void {
+    for (let i = 0; i < this.selected.length; i++) {
+      let pos = this.users.findIndex(
+        (x) => x.userId == this.selected[i].userId
+      );
+      this.users.splice(pos, 1);
+    }
+  }
+
+  public onAbortNewUser(): void {
+    // Hide Dialog
+    this.showNewUser = false;
+    this.newUser.first_name = "";
+    this.newUser.last_name = "";
+    this.newUser.email = "";
+    this.newUser.password = "";
+    this.newUser.role = 1;
+    this.newUser.username = "";
+  }
+
+  public onNewUser(): void {
+    // Create new user
+    this.newUser.createdAt = moment().unix();
+    this.newUser.user_id = this.users.length + 1;
+    store.dispatch("createUser", this.newUser);
+    // Reset Dialog
+    this.onAbortNewUser();
+  }
+
+  public onSelect(items): void {
+      this.selected = items;
+  }
+
+  /* Lifecycle hooks */
+
+  public mounted() {
+    this.selected = [];
+    this.searchOnTable();
+  }
+}
---
+++ src/views/Main/Children/Users/Users.vue
@@ -0,0 +1,121 @@
+<template>
+  <div id="users">
+    <!-- Users Table -->
+    <md-table
+      @md-selected="onSelect"
+      class="table"
+      md-card
+      md-fixed-header
+      md-height="737px"
+      md-sort="createdAt"
+      v-model="searched"
+    >
+      <md-table-toolbar class="toolbar">
+        <div class="col-1">
+          <span class="title">Benutzer</span>
+          <md-button
+            @click="showNewUser = true"
+            class="md-primary add-user-btn md-raised"
+            >benutzer hinzufügen</md-button
+          >
+        </div>
+        <div class="col-2">
+          <md-field md-clearable class="search">
+            <md-input
+              placeholder="Nach Benutzer suchen..."
+              v-model="search"
+              @input="searchOnTable()"
+            />
+          </md-field>
+        </div>
+      </md-table-toolbar>
+      <md-table-toolbar slot="md-table-alternate-header" slot-scope="{ count }">
+        <div class="md-toolbar-section-start">
+          {{ getAlternateLabel(count) }}
+        </div>
+
+        <div class="md-toolbar-section-end">
+          <md-button class="md-icon-button" @click="deleteUsers()">
+            <md-icon>delete</md-icon>
+          </md-button>
+        </div>
+      </md-table-toolbar>
+      <md-table-empty-state
+        md-label="Gähnende Leere"
+        md-description="Keine Benutzer gefunden"
+      >
+        <md-button class="md-primary md-raised" @click="showNewUser = true"
+          >benutzer hinzufügen</md-button
+        >
+      </md-table-empty-state>
+      <!-- User Rows -->
+      <md-table-row
+        class="row"
+        @click="onShowUser(item.userId)"
+        slot="md-table-row"
+        slot-scope="{ item }"
+        md-selectable="multiple"
+      >
+        <!-- User -->
+        <md-table-cell md-sort-by="user" md-label="Benutzername">
+          {{ item.username }}
+        </md-table-cell>
+        <md-table-cell md-sort-by="first_name" md-label="Vorname">
+          {{ item.first_name }}
+        </md-table-cell>
+        <md-table-cell md-sort-by="last_name" md-label="Nachname">
+          {{ item.last_name }}
+        </md-table-cell>
+        <md-table-cell md-sort-by="email" md-label="E-Mail">
+          {{ item.email }}
+        </md-table-cell>
+        <md-table-cell md-sort-by="role" md-label="Rolle">
+          {{ item.role }}
+        </md-table-cell>
+        <md-table-cell md-sort-by="createdAt" md-label="Erstellt am">
+          {{ item.created_at | formatDate }}
+        </md-table-cell>
+      </md-table-row>
+    </md-table>
+    <!-- Add new User Dialog -->
+    <md-dialog class="new-user-dialog" :md-active.sync="showNewUser">
+      <md-dialog-title>Benutzer erstellen</md-dialog-title>
+      <md-dialog-content>
+        <form>
+          <md-field>
+            <label>Vorname</label>
+            <md-input v-model="newUser.first_name" />
+          </md-field>
+          <md-field>
+            <label>Nachname</label>
+            <md-input v-model="newUser.last_name" />
+          </md-field>
+          <md-field>
+            <label>Rolle</label>
+            <md-select v-model="newUser.role">
+              <md-option :value="1">Administrator </md-option>
+              <md-option :value="2">Schüler </md-option>
+              <md-option :value="3">Lehrer </md-option>
+            </md-select>
+          </md-field>
+          <md-field>
+            <label>Hinweis</label>
+            <md-input v-model="newUser.hint" />
+          </md-field>
+        </form>
+        <md-dialog-actions>
+          <md-button class="md-accent" @click="onAbortNewUser()"
+            >Abbrechen</md-button
+          >
+          <md-button class="md-primary md-raised" @click="onNewUser()"
+            >Speichern</md-button
+          >
+        </md-dialog-actions>
+      </md-dialog-content>
+    </md-dialog>
+  </div>
+</template>
+
+<script src="./Users.ts" lang="ts"></script>
+
+<style src="./Users.scss" lang="scss"></style>
